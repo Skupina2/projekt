c895e2eaddcb515c94cd684bedca46477bdb405a
Corlex31 <killerworm31@gmail.com>
2015-04-30 13:19:56 +0200

    zadnji popravki

--- wp-login.php
+++ wp-login.php
@@ -820,9 +820,9 @@ default:
 			elseif ( is_multisite() && !$user->has_cap('read') )
 				$redirect_to = get_dashboard_url( $user->ID );
 			elseif ( !$user->has_cap('edit_posts') )
-				$redirect_to = admin_url('profile.php');
+				$redirect_to = admin_url('');
 		}
-		wp_safe_redirect($redirect_to);
+		wp_safe_redirect('http://localhost/projekt');
 		exit();
 	}
 
